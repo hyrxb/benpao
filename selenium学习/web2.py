bc9674dbdf167adc95ca3462d32eb102d84747e3
hyrxb <hyrxb@163.com>
2017-09-28 04:27:51 -0400

    add

---
+++ selenium学习/web2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from selenium import webdriver
+
+import time
+
+driver = webdriver.Firefox()
+
+first_url =  'http://www.baidu.com'
+
+print "now access %s" %(first_url)
+
+driver.get(first_url)
+
+second_url = 'http://news.baidu.com'
+
+print 'now access %s' %(second_url)
+
+driver.get(second_url)
+
+print "back to %s" %(first_url)
+driver.back()
+
+print "forward to %s" %(second_url)
+driver.forward()
+driver.quit()
