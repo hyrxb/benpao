e8477590bab30d63d3bf9f54480834b526a931d8
hyrxb <hyrxb@163.com>
2017-09-29 01:56:47 -0400

    添加注释

--- selenium学习/http_cookie1.py
+++ selenium学习/http_cookie1.py
@@ -3,16 +3,16 @@
 
 import cookielib
 import urllib2
-
+#设置保存cookie的文件
 filename = 'cookie.txt'
-
+#声明一个MozillaCookieJar对象实例来保存cookie,之后写入文件
 cookie = cookielib.MozillaCookieJar(filename)
-
+#利用urllib2库的HTTPCookieProcessor对象来创建cookie处理器
 handler = urllib2.HTTPCookieProcessor(cookie)
-
+#通过handler来构建opener
 opener = urllib2.build_opener(handler)
-
+#创建一个请求，原理同urllib2的urlopen
 response = opener.open('http://www.baidu.com')
-
+#保持cookie到文件
 cookie.save(ignore_discard=True,ignore_expires=True)
 
