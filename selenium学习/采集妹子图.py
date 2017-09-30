03023d87dc75832452b92bdebeb9a04e1ef64453
hyrxb <hyrxb@163.com>
2017-09-30 01:28:35 -0400

    添加注释

--- selenium学习/采集妹子图.py
+++ selenium学习/采集妹子图.py
@@ -11,6 +11,7 @@ import os
 def getHtml(url,headers):
 
 	#request  =urllib2.Request(url),headers=headers)  error
+	#这里需要对url请求地址进行编码，不然会报错
 	request  =urllib2.Request(url.encode('utf-8'),headers=headers)
 
 	response = urllib2.urlopen(request)
@@ -23,7 +24,7 @@ def getContent(url):
 	headers = {'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linu…) Gecko/20100101 Firefox/55.0','Referer':url}
 	html = getHtml(url,headers)
 	soup = BeautifulSoup(html,'html.parser')
-	a = soup.find_all('a')
+	a = soup.find_all('a') #查询所有的a标签
 	lst = []
 	for i in a:
 		href = i.attrs['href']
@@ -40,13 +41,14 @@ def savePic(url,imgPath):
 	div = soup.find('div',attrs={'class':'main-image'})
 	img  = div.find('img')
 	src =  img.attrs['src']
-
+	#妹子图有防盗链，这里设置了referer
 	headers = {'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linu…) Gecko/20100101 Firefox/55.0','Referer':url}
+	#抓取图片内容
 	pic_content = getHtml(src,headers)
-
+	#图片路径
 	file_name  = os.path.join(imgPath,str(int(round(time.time() * 1000))) +'.jpg')
 	print file_name
-
+	#保存图片到本地
 	with open(file_name,'wb') as f:
 	  	f.write(pic_content)
 	  	print('download end \n')
@@ -56,20 +58,24 @@ def getDetail(url):
 	html = getHtml(url,headers)
 	soup = BeautifulSoup(html,'html.parser')
 	tag = soup.find('div',attrs={'class':'main-meta'})
-
+	#查找妹子图片标签
 	title = tag.find('a').text
+	#按标签类型创建图片目录
 	imgPath = '/home/alex/'+title.encode('utf-8')
 	#print imgPath
-
+	#判断目录是否存在
 	if not os.path.isdir(imgPath):
 	 	os.mkdir(imgPath)
 
  	nav = soup.find('div',attrs={'class':'pagenavi'})
-	nav = str(nav)
+	nav = str(nav) #转换成字符串
+	#查找最后一个分页的页码数
 	result = re.findall(r"<span class=\"dots\">.*?</span><a.*?<span>(.*?)</span>",nav)
 	end =  int(result[0])
+	#遍历生成分页地址
 	for i in range(1,end):
 	 	src = url + '/' + str(i)
+		#下载图片
 		savePic(src,imgPath)
 
     
